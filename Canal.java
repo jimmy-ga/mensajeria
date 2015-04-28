d72fc77cdfcfbaa0d94bea7524c345e061a40269
marianaRamirez <marianaRamirez@users.noreply.github.com>
2015-04-28 17:30:19 -0600

    Delete Canal.java

--- Canal.java
+++
@@ -1,6 +0,0 @@
-public abstract class Canal
-{
-    public abstract void send(Object m); // envia un msj objeto
-    public abstract void send(); // senal para el receptor
-    public abstract Object receive(); // recibe un objeto
-}
