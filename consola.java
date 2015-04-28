d0b5e63afbd2dbe7cc45af3e70d7df0c9c7a2424
marianaRamirez <marianaRamirez@users.noreply.github.com>
2015-04-28 17:37:44 -0600

    Update consola.java

--- consola.java
+++ consola.java
@@ -1,12 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class consola //Clase para simular la consola de la aplicación
 {
-	public static void main(String args[])
+	public void crear() throws IOException
 	{
+                int directo =0; // 0 directo, 1 indirecto
+                int explicito =0;// 0 explicito, 1 implicito
+                int estatico =0; // 0 estatico, 1 dinamico
+                int largo =0;
+                int cola =0; // 0 fifo, 1 prioridad
+                BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Ingrese comando");
 		Boolean salida=false; //Variable para mantener el ciclo de ejecucion
 		while (!salida) //Inicio del ciclo de ejecución
 			{
-				String input = System.console().readLine(); //Lee el "comando" de la consola
+				String input = br.readLine(); //Lee el "comando" de la consola
 				salida=input.equals("salir"); //Compara con la palabra salir, se repite para otros comandos
 				System.out.println(salida); //Impresión para verificación
 			} //Si salida se vuelve true por ingresar ese comando el programa se cierra
